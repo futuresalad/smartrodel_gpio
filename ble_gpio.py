e9d38e6a3b5e631fa042f70f7740a1edcdfcc116
Ephraim Westenberger <ephriam.westenberger@gmail.com>
2023-01-09 13:04:50 +0100

    class ble

--- ble_gpio.py
+++ ble_gpio.py
@@ -1,5 +1,3 @@
-from gpiozero import LED, Button
-from ble_gpio import start_measurement
 from time import sleep
 import asyncio
 from bleak import BleakClient
@@ -8,71 +6,71 @@ import numpy as np
 import datetime
 import asyncio
 
-# BLE Device constants
-mac = "24:0A:C4:62:52:FE" #dev nxn
-RX_UUID = "6E400003-B5A3-F393-E0A9-E50E24DCCA9E"
-TX_UUID = "6E400002-B5A3-F393-E0A9-E50E24DCCA9E"
+class BLE():
+    def __init__(self):
+         
+        # BLE Device constants
+        self.mac = "24:0A:C4:62:52:FE" #dev nxn
+        self.RX_UUID = "6E400003-B5A3-F393-E0A9-E50E24DCCA9E"
+        self.TX_UUID = "6E400002-B5A3-F393-E0A9-E50E24DCCA9E"
 
-# BLE status
-connected = False
+        # BLE status
+        self.connected = False
 
-# Commands
-txOn = bytearray("on",'utf-8')
-txOff = bytearray("off",'utf-8')
+        # Commands
+        self.txOn = bytearray("on",'utf-8')
+        self.txOff = bytearray("off",'utf-8')
 
-# Dataframe to save measurements
-rec = np.zeros([1,5])
-dataframe = pd.DataFrame(rec, columns=['time', 'vl', 'vr', 'hl', 'hr'])
+        # Dataframe to save measurements
+        self.rec = np.zeros([1,5])
+        self.dataframe = pd.DataFrame(self.rec, columns=['time', 'vl', 'vr', 'hl', 'hr'])
 
-def bt_callback(sender: int, data: bytearray):
+def bt_callback(self, sender: int, data: bytearray):
     # Decode data from bytearrays to strings and split them at the "," delimiter
-    rec = data.decode("utf-8").split(",")
-    print(rec)
+    self.rec = data.decode("utf-8").split(",")
+    print(self.rec)
     # Convert every element of the row into an integer
-    for idx, element in enumerate(rec):
-        rec[idx] = int(element)
+    for idx, element in enumerate(self.rec):
+        self.rec[idx] = int(element)
 
     # Add that array as a row to the dataframe
-    dataframe.loc[len(dataframe)] = rec
+    self.dataframe.loc[len(self.dataframe)] = self.rec
 
 
-def export_data():
+def export_data(self):
         print("Exporting CSV")
-        dataframe.to_csv(path_or_buf=f"./data_export/data_{datetime.datetime.now().isoformat()}.csv", sep=',', index_label="Index", na_rep='NaN')
+        self.dataframe.to_csv(path_or_buf=f"./data_export/data_{datetime.datetime.now().isoformat()}.csv", sep=',', index_label="Index", na_rep='NaN')
 
-async def start_measurement(duration):
+async def start_measurement(self, duration):
 
-    global rec
-    global dataframe
-
-    rec = np.zeros([1,5])
-    dataframe = pd.DataFrame(rec, columns=['time', 'vl', 'vr', 'hl', 'hr'])
+    self.rec = np.zeros([1,5])
+    self.dataframe = pd.DataFrame(self.rec, columns=['time', 'vl', 'vr', 'hl', 'hr'])
 
     try:
-        async with BleakClient(mac) as client:
+        async with BleakClient(self.mac) as client:
                 
             if client.is_connected:
                     
-                    await client.start_notify(RX_UUID, bt_callback)
-                    await client.write_gatt_char(TX_UUID, bytearray(str(duration),'utf-8'))
-                    await client.write_gatt_char(TX_UUID, txOn)
+                    await client.start_notify(self.RX_UUID, bt_callback)
+                    await client.write_gatt_char(self.TX_UUID, bytearray(str(duration),'utf-8'))
+                    await client.write_gatt_char(self.TX_UUID, self.txOn)
                     sleep(duration)
-                    await client.write_gatt_char(TX_UUID, txOff)
+                    await client.write_gatt_char(self.TX_UUID, self.txOff)
                     client.disconnect()
                     print(f"Client connected: {client.is_connected()}")
-                    success = True
+                    self.success = True
                     
                     
             else:
-                print(f'BT Device with MAC {mac} not found')    
+                print(f'BT Device with MAC {self.mac} not found')    
 
     except Exception as e:
 
         print(e)
-        success = False
+        self.success = False
                    
-    if success:
-        export_data()
+    if self.success:
+        self.export_data()
         print("Success!")
 
     else:
