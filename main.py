b3cf94427baaf626d96a9c81961376fc560a053c
Ephraim Westenberger <ephriam.westenberger@gmail.com>
2023-01-09 11:11:04 +0100

    added ble

--- main.py
+++ main.py
@@ -1,5 +1,13 @@
 import RPi.GPIO as GPIO
 from time import sleep
+from ble import bt_daq
+import asyncio
+
+# BLE Device constants
+mac = "24:0A:C4:62:52:FE" #dev nxn
+
+# BLE status
+connected = False
 
 GPIO.setmode(GPIO.BCM)
 GPIO.setwarnings(False)
@@ -21,11 +29,43 @@ for i in range(5):
     sleep(.1)
 
 def callback(ev=None):
+
+    for i in range(5):
+        GPIO.output(led_pin, GPIO.HIGH)
+        sleep(.1)
+        GPIO.output(led_pin, GPIO.LOW)
+        sleep(.1)
+
     global led_on
     led_on = not led_on
     GPIO.output(led_pin, GPIO.HIGH if led_on else GPIO.LOW)
+    asyncio.run(start_measurement())
 
+ESP32_1 = bt_daq(mac)
 GPIO.add_event_detect(btn_pin, GPIO.RISING, callback=callback, bouncetime=300)
 
+async def start_measurement():
+            success = await ESP32_1.get_data(2)
+
+            if success:
+                ESP32_1.export_data()
+
+                for i in range(5):
+                    GPIO.output(led_pin, GPIO.HIGH)
+                    sleep(.5)
+                    GPIO.output(led_pin, GPIO.LOW)
+                    sleep(.1)
+
+            else:
+                print("not successful")
+
+                for i in range(3):
+
+                    GPIO.output(led_pin, GPIO.HIGH)
+                    sleep(1)
+                    GPIO.output(led_pin, GPIO.LOW)
+                    sleep(1)
+
 while True:
-    sleep(1)
+    sleep(5)
+    print("Awaiting Start..")
