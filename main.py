5a3036139ea43be909e6a28644301a349c6b8770
Ephraim Westenberger <ephriam.westenberger@gmail.com>
2023-01-09 10:48:34 +0100

    I/O

--- main.py
+++ main.py
@@ -1,15 +1,31 @@
 import RPi.GPIO as GPIO
+from time import sleep
+
 GPIO.setmode(GPIO.BCM)
+GPIO.setwarnings(False)
 
-GPIO.setup(5, GPIO.OUT)
-GPIO.output(5, GPIO.HIGH)
+# LED Output pin
+GPIO.setup(16, GPIO.OUT, initial=GPIO.LOW)
 
-GPIO.setup(6, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+# Initial state
+led_on = False
+led_pin = 16
+# Button
+GPIO.setup(25, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+btn_pin = 25
 
+for i in range(5):
+    GPIO.output(led_pin, GPIO.HIGH)
+    sleep(.1)
+    GPIO.output(led_pin, GPIO.LOW)
+    sleep(.1)
+
+def callback(ev=None):
+    global led_on
+    led_on = not led_on
+    GPIO.output(led_pin)
+
+GPIO.add_event_detect(23, GPIO.RISING, callback=callback, bouncetime=300)
 
 while True:
-    if GPIO.input(5):
-        print("Pin 11 is HIGH")
-
-    else:
-        print("Pin 11 is LOW")
+    sleep(1)
