da963fe4f85e1f72d43881a1e118459ab9cf5566
Ephraim Westenberger <ephriam.westenberger@gmail.com>
2023-01-27 12:59:46 +0100

    acd update

--- adc.py
+++ adc.py
@@ -1,27 +1,36 @@
 import time
 import Adafruit_MCP3008 as MCP
+import Adafruit_GPIO.SPI as SPI
 
-# Pin config:
+# MCP3008 Pinout
+# CH0 -|1 * 16|- VDD
+# CH1 -|2   15|- Vref
+# CH2 -|3   14|- AGND
+# CH3 -|4   13|- CLK
+# CH4 -|5   12|- Dout
+# CH5 -|6   11|- Din_
+# CH6 -|7   10|- CS
+# CH7 -|8 __ 9|- DGND
 
-# CS GPIO8 (Pin 24)
-# VDD, VREF 3.3V (Pin 17)
-# AGND, DGND GND (Pin 25, 20)
-# MOSI GPIO10 (Pin 19)
-# MISO GPIO9 (Pin 21)
-# CLK GPIO11 (Pin 23)
-# GND (Pin 25)
+# Raspberry Pi Pinout
+# CS -> GPIO25 (Pin 22)
+# VDD -> VREF 3.3V (Pin 17)
+# AGND, DGND -> GND (Pin 25, 20)
+# MOSI -> GPIO10 (Pin 19)
+# MISO -> GPIO9 (Pin 21)
+# CLK -> GPIO11 (Pin 23)
+
 
 class ADC():
     def __init__(self):
 
         # Hardware SPI configuration:
-        self.SPI_PORT = 0
-        self.SPI_DEVICE = 0
+        SPI_PORT = 0
+        SPI_DEVICE = 0
         self.mcp = MCP.MCP3008(spi=SPI.SpiDev(SPI_PORT, SPI_DEVICE))
 
     def read(self):
 
-        # Initialize array with 8 zeroes
         values = []
 
         # Read values of 8 channels
