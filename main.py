2eb1ca30b21cc6518de1d014e27aebce22c3d935
Ephraim Westenberger <ephraim.westenberger@gmail.com>
2023-01-30 15:45:42 +0100

    added files to main

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import time
 import smbus
 import numpy as np
 import spidev
+import cv2 as cv
 import RPi.GPIO as GPIO
 
 from imusensor.MPU9250 import MPU9250
@@ -13,14 +14,20 @@ CS_ADC = 12
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(CS_ADC, GPIO.OUT)
 
+class CAM():
+    def __init__(self):
+        self.cap = cv.VideoCapture(0)
+        width = int(self.cap.get(cv.CAP_PROP_FRAME_WIDTH))
+        height = int(self.cap.get(cv.CAP_PROP_FRAME_HEIGHT))
+        fourcc = cv.VideoWriter_fourcc('m', 'p', '4', 'v')
+        self.writer = cv.VideoWriter('testvid.mp4', fourcc, 30, (width, height))
+
 class IMU():
     def __init__(self):
         address = 0x68
         bus = smbus.SMBus(1)
         self.imu = MPU9250.MPU9250(bus, address)
-        imu.begin()
-
-
+        
 class ADC():
     def __init__(self):
         
@@ -34,15 +41,54 @@ class ADC():
         data = ((adc[1]&3) << 8) + adc[2]
         return data
 
+cam = CAM()
 
-imu = IMU()
+if not cam.cap.isOpened():
+    print("Cannot open camera")
+    exit()
+
+sensor = IMU()
+sensor.imu.begin()
+
 adc = ADC()
 
+delay = 2 * 1_000_000 
+starttime = time.time_ns()
+lastread = starttime
+readcount = 0
+
 while True:
-	imu.readSensor()
-	imu.computeOrientation()
-
-    for channel in range(8):
-        print(adc.read(channel))
-
-    print("roll: {0} ; pitch : {1} ; yaw : {2}".format(imu.roll, imu.pitch, imu.yaw))
+    
+    # Capture frame-by-frame
+    ret, frame = cam.cap.read()
+    
+    # write frame to file    
+    cam.writer.write(frame)
+    
+    # Display the resulting frame
+    #cv.imshow('frame', frame)
+    
+    if cv.waitKey(1) == ord('q'):
+        break
+        
+    if ((time.time_ns()) - lastread) > delay:
+        
+        lastread = time.time_ns()
+        seconds = ((lastread-starttime) / 1_000_000_000)
+        sensor.imu.readSensor()
+        sensor.imu.computeOrientation()
+        adc_val = np.zeros(8)
+        
+        readcount = readcount+1
+        
+        for channel in range(8):
+            adc_val[channel] = adc.read(channel)
+        
+        #print(adc_val)
+        #print("roll: {0} ; pitch : {1} ; yaw : {2}".format(sensor.imu.roll, sensor.imu.pitch, sensor.imu.yaw))
+        if seconds > 10:
+            print(f'readcount: {readcount}')
+            break
+cam.cap.release()
+cam.writer.release()
+cv.destroyAllWindows()
